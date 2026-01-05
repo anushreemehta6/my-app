aed831138963bed3d40a5dbb972baf1359e4e61c
Anushree Mehta <anushreemehta6@gmail.com>
2026-01-05 23:52:12 +0530

    renderd components on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
+import About from "./component/about";
+import Hero from "./component/hero";
 
 export default function Home() {
   return (
-   <></>
+    <>
+      <div
+        className="bg-linear-to-r from-[#1E1E1E_8%] to-[#1E1E1E_75%]
+"
+      >
+        <Hero />
+        <About />
+      </div>
+    </>
   );
 }
