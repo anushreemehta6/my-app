7932941b2ff25fe3ad4b13d1faa2852ebb0c463c
Anushree Mehta <anushreemehta6@gmail.com>
2026-01-15 16:25:13 +0530

    experience data added

---
+++ src/app/constants/expirence.ts
@@ -0,0 +1,104 @@
+import { details } from "motion/react-client";
+
+export const expirence = [
+  {
+    phase: "1",
+    name: "Developer Relations & Content (DevRel)",
+    startDate: "January 2025",
+    endDate: "August 2025",
+    duration: "8+ Months",
+    content: [
+      {
+        id: "1",
+        detail: "Drafted WhitePaper for Fluxor ",
+      },
+      {
+        id: "2",
+        detail: "Authored technical and educational blogs around Web3",
+      },
+      {
+        id: "3",
+        detail:
+          "Hosted and moderated virtual events, community sessions, and Twitter Spaces, engaging developers and Web3 enthusiasts",
+      },
+      {
+        id: "4",
+        detail: "Drafted WhitePaper for Conscious Club  ",
+      },
+      {
+        id: "5",
+        detail: " managed Fluxor’s social media presence",
+      },
+      {
+        id: "6",
+        details: "Wrote PR content",
+      },
+    ],
+    footer: [
+      {
+        name: "Technical Writing",
+      },
+      {
+        name: " Web3 Education",
+      },
+      {
+        name: "Developer Relations ",
+      },
+      {
+        name: "Community & Events",
+      },
+      {
+        name: "Social Growth",
+      },
+    ],
+  },
+  {
+    phase: "2",
+    name: "Full stack developer",
+    startDate: "August 2025",
+    endDate: "Currently working",
+    content: [
+      {
+        details:
+          "Developed and enhanced Fluxor’s main website, focusing heavily on UI/UX and frontend architecture.",
+      },
+      {
+        details: "Built role-based dashboards for: Sponsors,Organizers etc.",
+      },
+      {
+        details:
+          "Designed and implemented a UTM Tracking System to track campaign performance ",
+      },
+      {
+        details:
+          "Worked the development of Fluxor Consultancy Website, delivering a clean, professional, and conversion-focused interface.",
+      },
+      {
+        details:
+          "Collaborated cross-functionally with design, marketing, and product teams to ensure seamless user experiences.",
+      },
+    ],
+    continue: "The experience adds on everyday....",
+    footer: [
+      {
+        name: "React",
+      },
+      {
+        name: "JavaScript",
+      },
+      {
+        name: "TypeScript",
+      },
+      {
+        name: "Express js",
+      },
+      {
+        name: "Node js",
+      },
+      {
+        name: "UX/UI",
+      },
+      { name: "NeonDB" },
+    ],
+  },
+];
